7d67317acb5135f459b9e5fb9471926d83625756
bobmagicii <bob@majdak.net>
2022-08-22 00:50:45 +0000

    new Datafilter object

---
+++ src/Nether/Object/Struct/DatafilterCallable.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Nether\Object\Struct;
+
+use Nether\Object\Datafilter;
+
+class DatafilterCallable {
+
+	public mixed
+	$Func;
+
+	public ?array
+	$Argv;
+
+	public function
+	__Construct(callable $Func, ?array $Argv=NULL) {
+
+		$this->Func = $Func;
+		$this->Argv = $Argv;
+
+		return;
+	}
+
+	public function
+	__Invoke(mixed $Val, string $Key, Datafilter $Input):
+	mixed {
+
+		return ($this->Func)($Val, $Key, $Input, ...($this->Argv ?? []));
+	}
+
+	public function
+	__DebugInfo():
+	array {
+
+		return [
+			'Func' => is_callable($this->Func),
+			'Argv' => $this->Argv
+		];
+	}
+
+}
---
+++ tests/Nether/Object/DatafilterTest.php
@@ -0,0 +1,211 @@
+<?php
+
+namespace NetherTestSuite\Object\Datafilter;
+
+use Exception;
+use PHPUnit\Framework\TestCase;
+use Nether\Object\Datafilter;
+
+class DatafilterTest
+extends TestCase {
+
+	/** @test */
+	public function
+	TestReadWrite():
+	void {
+
+		$Data = [ 'One'=> 1, 'Two'=> 2 ];
+		$Filter = new Datafilter($Data);
+
+		// basic reading.
+
+		$this->AssertIsInt($Filter->One);
+		$this->AssertIsInt($Filter->Two);
+		$this->AssertEquals(1, $Filter->One);
+		$this->AssertEquals(2, $Filter->Two);
+		$this->AssertNull($Filter->Zero);
+
+		// basic writing.
+
+		$Filter->Three = 3;
+		$this->AssertIsInt($Filter->Three);
+		$this->AssertEquals(3, $Filter->Three);
+
+		// test cache clear on write.
+
+		$Filter['Three'] = 3;
+		$Filter->Three(fn($Val)=> (float)$Val);
+		$this->AssertIsFloat($Filter->Three);
+		$this->AssertEquals(3, $Filter->Three);
+		$this->AssertTrue($Filter->CacheHas('Three'));
+
+		// test countable
+
+		$this->AssertEquals(3, count($Filter));
+
+		// test offset Get and Unset
+
+		$this->AssertTrue(isset($Filter['One']));
+
+		unset($Filter['One']);
+		$this->AssertFalse(isset($Filter['One']));
+		$this->AssertNull($Filter['One']);
+
+		return;
+	}
+
+	/** @test */
+	public function
+	TestFiltering():
+	void {
+
+		$Data = [ 'One'=> 1, 'Two'=> 2 ];
+		$Filter = new Datafilter($Data);
+
+		$Filter
+		->Zero(fn($Val): bool => TRUE)
+		->One(fn($Val): string => (string)$Val)
+		->Two(fn($Val): float => (float)$Val);
+
+		$this->AssertIsString($Filter->One);
+		$this->AssertTrue($Filter->One === '1');
+
+		$this->AssertIsFloat($Filter->Two);
+		$this->AssertTrue($Filter->Two === 2.0);
+
+		$this->AssertNull($Filter->Zero);
+
+		return;
+	}
+
+	/** @test */
+	public function
+	TestIteration():
+	void {
+
+		$Data = [ 'One'=> 1, 'Two'=> 2, 'Three'=> 3, 'Four'=> 4 ];
+		$Keys = array_keys($Data);
+
+		$Filter = new Datafilter($Data);
+		$Key = NULL;
+		$Val = NULL;
+
+		$Iter = 1;
+		foreach($Filter as $Key => $Val) {
+			$this->AssertEquals($Val, $Iter);
+			$this->AssertEquals($Key, strtolower($Keys[$Iter - 1]));
+
+			$Iter++;
+		}
+
+		$Iter = 1;
+		foreach($Data as $Key => $Val) {
+			$this->AssertEquals($Filter[$Key], $Iter);
+
+			$Iter++;
+		}
+
+		return;
+	}
+
+	/** @test */
+	public function
+	TestUncallable():
+	void {
+
+		$Data = [ 'One'=> 1, 'Two'=> 2, 'Three'=> 3, 'Four'=> 4 ];
+		$Filter = new Datafilter($Data);
+		$HadExcept = FALSE;
+
+		try {
+			$Filter->One('func_does_not_exist_my_dudes');
+		}
+
+		catch(Exception $Err) {
+			$HadExcept = TRUE;
+		}
+
+		$this->AssertTrue($HadExcept);
+
+		return;
+	}
+
+	/** @test */
+	public function
+	TestCallableWithMoreArgs():
+	void {
+
+		$Data = [ 'One'=> 1, 'Two'=> 2, 'Three'=> 3, 'Four'=> 4 ];
+		$Filter = new Datafilter($Data);
+		$Key = NULL;
+		$Val = NULL;
+		$MinMax = function($Val, $Key, $Input, int $Min, int $Max) {
+			if($Val < $Min)
+			return $Min;
+
+			if($Val > $Max)
+			return $Max;
+
+			return $Val;
+		};
+
+		$Filter->One($MinMax, 2, 3);
+		$Filter->Two($MinMax, 2, 3);
+		$Filter->Three($MinMax, 2, 3);
+		$Filter->Four($MinMax, 2, 3);
+
+		foreach($Filter as $Key => $Val)
+		$this->AssertEquals(
+			match($Key){
+				'one'   => 2,
+				'two'   => 2,
+				'three' => 3,
+				'four'  => 3
+			},
+			$Val
+		);
+
+		return;
+	}
+
+	/** @test */
+	public function
+	TestCaseSensitivity():
+	void {
+
+		$Data = [ 'One'=> 1, 'Two'=> 2, 'Three'=> 3, 'Four'=> 4 ];
+		$Filter = new Datafilter($Data, Case: TRUE);
+
+		$this->AssertEquals(1, $Filter->One);
+		$this->AssertNull($Filter->one);
+
+		$Filter->SetCaseSensitive(FALSE);
+		$this->AssertEquals(1, $Filter->One);
+		$this->AssertEquals(1, $Filter->one);
+
+		return;
+	}
+
+	/** @test */
+	public function
+	TestCacheToggle():
+	void {
+
+		$Data = [ 'One'=> 1, 'Two'=> 2, 'Three'=> 3, 'Four'=> 4 ];
+
+		$Filter = new Datafilter($Data, Cache: FALSE);
+		$this->AssertEquals(1, $Filter->One);
+		$this->AssertFalse($Filter->CacheHas('One'));
+
+		$Filter->SetCacheOutput(TRUE);
+		$this->AssertEquals(1, $Filter->One);
+		$this->AssertTrue($Filter->CacheHas('One'));
+
+		$Filter->SetCacheOutput(FALSE);
+		$this->AssertEquals(1, $Filter->One);
+		$this->AssertFalse($Filter->CacheHas('One'));
+
+		return;
+	}
+
+}
